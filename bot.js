98dfe8bd5dbb4bee2f3129c9dd45ebaa8ee34f3c
matesa <75279583+matesa@users.noreply.github.com>
2022-04-15 01:06:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,256 +5,183 @@ you may not use this file except in compliance with the License.
 
 MatesaWhatsapp - Poyraz
 */
-
-const fs = require("fs");
-const path = require("path");
-const events = require("./events");
-const chalk = require('chalk');
-const config = require('./config');
-const {WAConnection, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
-const {Message, StringSession, Image, Video} = require('./matesawhatsapp/');
-const { DataTypes } = require('sequelize');
-const { GreetingsDB, getMessage } = require("./plugins/sql/greetings");
-const got = require('got');
+const fs = require("fs")
+const path = require("path")
+const { handleMessages } = require("./Utilis/msg")
+const chalk = require("chalk")
+const { DataTypes } = require("sequelize")
+const config = require("./config")
+const { WAConnection, MessageType } = require("@adiwajshing/baileys")
+const { StringSession } = require("./Utilis/matesawhatsapp")
+const { getJson } = require("./Utilis/download")
+const { customMessageScheduler } = require("./Utilis/schedule")
+const { prepareGreetingMedia } = require("./Utilis/greetings")
+const { groupMuteSchuler, groupUnmuteSchuler } = require("./Utilis/groupmute")
+const { PluginDB } = require("./plugins/sql/plugin")
 
 // Sql
-const MatesaWhatsappDB = config.DATABASE.define('matesa', {
-    info: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    value: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    }
-});
+const got = require("got")
+const { startMessage, waWebVersion } = require("./Utilis/Misc")
+const MatesaWhatsappDB = config.DATABASE.define("MatesaWhatsapp", {
+  info: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  value: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+})
 
-fs.readdirSync('./plugins/sql/').forEach(plugin => {
-    if(path.extname(plugin).toLowerCase() == '.js') {
-        require('./plugins/sql/' + plugin);
-    }
-});
-
-const plugindb = require('./plugins/sql/plugin');
+fs.readdirSync("./plugins/sql/").forEach((plugin) => {
+  if (path.extname(plugin).toLowerCase() == ".js") {
+    require("./plugins/sql/" + plugin)
+  }
+})
 
 // Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
 String.prototype.format = function () {
-    var i = 0, args = arguments;
-    return this.replace(/{}/g, function () {
-      return typeof args[i] != 'undefined' ? args[i++] : '';
-    });
-};
+  var i = 0,
+    args = arguments
+  return this.replace(/{}/g, function () {
+    return typeof args[i] != "undefined" ? args[i++] : ""
+  })
+}
 
 if (!Date.now) {
-    Date.now = function() { return new Date().getTime(); }
+  Date.now = function () {
+    return new Date().getTime()
+  }
 }
 
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
+Array.prototype.remove = function () {
+  var what,
+    a = arguments,
+    L = a.length,
+    ax
+  while (L && this.length) {
+    what = a[--L]
+    while ((ax = this.indexOf(what)) !== -1) {
+      this.splice(ax, 1)
     }
-    return this;
-};
+  }
+  return this
+}
 
-async function MatesaWhatsapp () {
-    await config.DATABASE.sync();
-    var StrSes_Db = await MatesaWhatsappDB.findAll({
-        where: {
-          info: 'StringSession'
-        }
-    });
-    
-    const conn = new WAConnection();
-    const Session = new StringSession();
+async function MatesaWhatsapp(version) {
+  await config.DATABASE.sync()
+  let StrSes_Db = await MatesaWhatsappDB.findAll({
+    where: {
+      info: "StringSession",
+    },
+  })
+  const conn = new WAConnection()
+  conn.version = version
+  const Session = new StringSession()
+  conn.logger.level = config.DEBUG ? "debug" : "warn"
+  var nodb
 
-    conn.logger.level = config.DEBUG ? 'debug' : 'warn';
-    var nodb;
+  if (StrSes_Db.length < 1 || config.CLR_SESSION) {
+    nodb = true
+    conn.loadAuthInfo(Session.deCrypt(config.SESSION))
+  } else {
+    conn.loadAuthInfo(Session.deCrypt(StrSes_Db[0].dataValues.value))
+  }
 
+  conn.on("connecting", () => {
+    console.log(`${chalk.red.bgBlack("B")}${chalk.green.bgBlack(
+      "o"
+    )}${chalk.blue.bgBlack("t")}${chalk.yellow.bgBlack(
+      "t"
+    )}${chalk.white.bgBlack("u")}${chalk.magenta.bgBlack("s")}
+${chalk.white.bold.bgBlack("Version:")} ${chalk.red.bold.bgBlack(
+      config.VERSION
+    )}
+${chalk.blue.italic.bgBlack("ℹ️ WhatsApp'a bağlanılıyor... Lütfen bekleyin.")}`)
+  })
+  conn.on("open", async () => {
+    console.log(chalk.green.bold("✅ Giriş başarılı!"))
+    console.log(chalk.blueBright.italic("⬇️ Pluginler yükleniyor..."))
+    console.log(chalk.blueBright.italic("✅ Giriş bilgileri güncellendi!"))
+
+    const authInfo = conn.base64EncodedAuthInfo()
     if (StrSes_Db.length < 1) {
-        nodb = true;
-        conn.loadAuthInfo(Session.deCrypt(config.SESSION)); 
+      await MatesaWhatsappDB.create({
+        info: "StringSession",
+        value: Session.createStringSession(authInfo),
+      })
     } else {
-        conn.loadAuthInfo(Session.deCrypt(StrSes_Db[0].dataValues.value));
+      await StrSes_Db[0].update({
+        value: Session.createStringSession(authInfo),
+      })
     }
 
-    conn.on ('credentials-updated', async () => {
-        console.log(
-            chalk.blueBright.italic('✅ Login information updated!')
-        );
-
-        const authInfo = conn.base64EncodedAuthInfo();
-        if (StrSes_Db.length < 1) {
-            await MatesaWhatsappDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
-        } else {
-            await StrSes_Db[0].update({ value: Session.createStringSession(authInfo) });
+    let plugins = await PluginDB.findAll()
+    plugins.map(async (plugin) => {
+      try {
+        if (!fs.existsSync("./plugins/" + plugin.dataValues.name + ".js")) {
+          console.log(plugin.dataValues.name)
+          let response = await got(plugin.dataValues.url)
+          if (response.statusCode == 200) {
+            fs.writeFileSync(
+              "./plugins/" + plugin.dataValues.name + ".js",
+              response.body
+            )
+            require("./plugins/" + plugin.dataValues.name + ".js")
+          }
         }
-    })    
-
-    conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Matesa')}
-${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
-
-${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
-    });
-    
-
-    conn.on('open', async () => {
+      } catch (error) {
         console.log(
-            chalk.green.bold('✅ Login successful!')
-        );
-
-        console.log(
-            chalk.blueBright.italic('⬇️ Installing external plugins...')
-        );
-
-        var plugins = await plugindb.PluginDB.findAll();
-        plugins.map(async (plugin) => {
-            if (!fs.existsSync('./plugins/' + plugin.dataValues.name + '.js')) {
-                console.log(plugin.dataValues.name);
-                var response = await got(plugin.dataValues.url);
-                if (response.statusCode == 200) {
-                    fs.writeFileSync('./plugins/' + plugin.dataValues.name + '.js', response.body);
-                    require('./plugins/' + plugin.dataValues.name + '.js');
-                }     
-            }
-        });
-
-        console.log(
-            chalk.blueBright.italic('⬇️  Installing plugins...')
-        );
-
-        fs.readdirSync('./plugins').forEach(plugin => {
-            if(path.extname(plugin).toLowerCase() == '.js') {
-                require('./plugins/' + plugin);
-            }
-        });
-
-        console.log(
-            chalk.green.bold('✅ Plugins installed!')
-        );
-    });
-    
-    conn.on('message-new', async msg => {
-        if (msg.key && msg.key.remoteJid == 'status@broadcast') return;
-
-        if (config.NO_ONLINE) {
-            await conn.updatePresence(msg.key.remoteJid, Presence.unavailable);
-        }
-
-        if (msg.messageStubType === 32 || msg.messageStubType === 28) {
-            // Görüşürüz Mesajı
-            var gb = await getMessage(msg.key.remoteJid, 'goodbye');
-            if (gb !== false) {
-                await conn.sendMessage(msg.key.remoteJid, gb.message, MessageType.text);
-            }
-            return;
-        } else if (msg.messageStubType === 27 || msg.messageStubType === 31) {
-            // Hoşgeldin Mesajı
-            var gb = await getMessage(msg.key.remoteJid);
-            if (gb !== false) {
-                await conn.sendMessage(msg.key.remoteJid, gb.message, MessageType.text);
-            }
-            return;
-        }
-
-        events.commands.map(
-            async (command) =>  {
-                if (msg.message && msg.message.imageMessage && msg.message.imageMessage.caption) {
-                    var text_msg = msg.message.imageMessage.caption;
-                } else if (msg.message && msg.message.videoMessage && msg.message.videoMessage.caption) {
-                    var text_msg = msg.message.videoMessage.caption;
-                } else if (msg.message) {
-                    var text_msg = msg.message.extendedTextMessage === null ? msg.message.conversation : msg.message.extendedTextMessage.text;
-                } else {
-                    var text_msg = undefined;
-                }
-
-                if ((command.on !== undefined && (command.on === 'image' || command.on === 'photo')
-                    && msg.message && msg.message.imageMessage !== null && 
-                    (command.pattern === undefined || (command.pattern !== undefined && 
-                        command.pattern.test(text_msg)))) || 
-                    (command.pattern !== undefined && command.pattern.test(text_msg)) || 
-                    (command.on !== undefined && command.on === 'text' && text_msg) ||
-                    // Video
-                    (command.on !== undefined && (command.on === 'video')
-                    && msg.message && msg.message.videoMessage !== null && 
-                    (command.pattern === undefined || (command.pattern !== undefined && 
-                        command.pattern.test(text_msg))))) {
-
-                    let sendMsg = false;
-                    var chat = conn.chats.get(msg.key.remoteJid)
-                        
-                    if ((config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
-                        (msg.participant && config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.SUDO || config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.SUDO)
-                    ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
-                        if (command.onlyPinned && chat.pin === undefined) return;
-                        if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
-                        else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
-                    }
-    
-                    if (sendMsg) {
-                        if (config.SEND_READ && command.on === undefined) {
-                            await conn.chatRead(msg.key.remoteJid);
-                        }
-                        
-                        var match = text_msg.match(command.pattern);
-                        
-                        if (command.on !== undefined && (command.on === 'image' || command.on === 'photo' )
-                        && msg.message.imageMessage !== null) {
-                            whats = new Image(conn, msg);
-                        } else if (command.on !== undefined && (command.on === 'video' )
-                        && msg.message.videoMessage !== null) {
-                            whats = new Video(conn, msg);
-                        } else {
-                            whats = new Message(conn, msg);
-                        }
-
-                        if (command.deleteCommand && msg.key.fromMe) {
-                            await whats.delete(); 
-                        }
-
-                        try {
-                            await command.function(whats, match);
-                        } catch (error) {
-                            if (config.LANG == 'TR' || config.LANG == 'AZ') {
-                                await conn.sendMessage(conn.user.jid, '*-- HATA RAPORU [MATESA] --*' + 
-                                    '\n*Matesa bir hata gerçekleşti!*'+
-                                    '\n_Bu hata logunda numaranız veya karşı bir tarafın numarası olabilir. Lütfen buna dikkat edin!_' +
-                                    '\n_Bu mesaj sizin numaranıza (kaydedilen mesajlar) gitmiş olmalıdır._\n\n' +
-                                    '*Gerçekleşen Hata:* ```' + error + '```\n\n'
-                                    , MessageType.text);
-                            } else {
-                                await conn.sendMessage(conn.user.jid, '*-- ERROR REPORT [MATESA] --*' + 
-                                    '\n*Matesa an error has occurred!*'+
-                                    '\n_This error log may include your number or the number of an opponent. Please be careful with it!_' +
-                                    '\n_This message should have gone to your number (saved messages)._\n\n' +
-                                    '*Error:* ```' + error + '```\n\n'
-                                    , MessageType.text);
-                            }
-                        }
-                    }
-                }
-            }
+          `pluginler yüklenemedi : ${plugin.dataValues.name}`
         )
-    });
+      }
+    })
+    console.log(chalk.blueBright.italic("⬇️  pluginler yükleniyor..."))
 
-    try {
-        await conn.connect();
-    } catch {
-        if (!nodb) {
-            console.log(chalk.red.bold('Eski sürüm stringiniz yenileniyor...'))
-            conn.loadAuthInfo(Session.deCrypt(config.SESSION)); 
-            try {
-                await conn.connect();
-            } catch {
-                return;
-            }
-        }
-    }
+    fs.readdirSync("./plugins").forEach((plugin) => {
+      if (path.extname(plugin).toLowerCase() == ".js") {
+        require("./plugins/" + plugin)
+      }
+    })
+
+    console.log(chalk.green.bold("✅ Pluginler yüklendi!"))
+    await conn.sendMessage(
+      conn.user.jid,
+      await startMessage(),
+      MessageType.text,
+      { detectLinks: false }
+    )
+  })
+  conn.on("close", (e) => console.log(e.reason))
+
+  await groupMuteSchuler(conn)
+  await groupUnmuteSchuler(conn)
+  await customMessageScheduler(conn)
+
+  conn.on("chat-update", (m) => {
+    if (!m.hasNewMessage) return
+    if (!m.messages && !m.count) return
+    const { messages } = m
+    const all = messages.all()
+    handleMessages(all[0], conn)
+  })
+
+  try {
+    await conn.connect()
+  } catch (e) {
+    if (!nodb) {
+      console.log(chalk.red.bold("Eski versiyon stringiniz yenileniyor..."))
+      conn.loadAuthInfo(Session.deCrypt(config.SESSION))
+      try {
+        await conn.connect()
+      } catch (e) {
+        return
+      }
+    } else console.log(`${e.message}`)
+  }
 }
 
-Matesa();
+;(async () => {
+  await prepareGreetingMedia()
+  matesaWhatsapp(await waWebVersion())
+})()
